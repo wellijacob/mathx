86a2476835d5436273bdedc4b3b7f027a8de8045
wellijacob <wellijacob@gmail.com>
2024-12-04 23:39:37 -0300

    005

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -26,7 +26,62 @@ class MainController extends Controller
 
         ]);
 
-        dd($request->all());
+        // get selected operations
+        $operations = [];
+        $operations[] = ($request->check_sum ?? null) ? 'sum' : null;
+        $operations[] = ($request->check_subtraction ?? null) ? 'subtraction' : null;
+        $operations[] = ($request->check_multiplication ?? null) ? 'multiplication' : null;
+        $operations[] = ($request->check_division ?? null) ? 'division' : null;
+
+        // get numbers (min end max)
+        $min = $request->number_one;
+        $max = $request->number_two;
+
+        // get number of exercises
+        $numberExercises = $request->number_exercises;
+
+        // generation exercises
+        $exercises = [];
+        for ($index = 1; $index <= $numberExercises; $index++) {
+
+            $operation = $operations[array_rand($operations)];
+            $number1 = rand($min, $max);
+            $number2 = rand($min, $max);
+
+            $exercise = '';
+            $solution = '';
+
+            switch ($operation) {
+                case 'sum':
+                    $exercise = "$number1 + $number2 =";
+                    $solution = $number1 + $number2;
+                    break;
+
+                case 'subtraction':
+                    $exercise = "$number1 - $number2 =";
+                    $solution = $number1 - $number2;
+                    break;
+
+                case 'multiplication':
+                    $exercise = "$number1 * $number2 =";
+                    $solution = $number1 * $number2;
+                    break;
+
+                case 'division':
+                    $exercise = "$number1 / $number2 =";
+                    $solution = $number1 / $number2;
+                    break;
+            }
+
+            $exercises[] = [
+                'exercise_number' => $index,
+                'exercise' => $exercise,
+                'solution' => "$exercise $solution"
+            ];
+        }
+
+        //dd($request->all());
+        dd($exercises);
     }
 
     public function printExercises()
